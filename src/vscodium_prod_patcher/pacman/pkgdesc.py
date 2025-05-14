6226f2fd758e3cd60066eec0c7bf38da9b80b57d
fnrir <fnr1r0@protonmail.com>
2025-05-14 18:28:12 +0200

    fix: cleanup package_desc_read

--- src/vscodium_prod_patcher/pacman/pkgdesc.py
+++ src/vscodium_prod_patcher/pacman/pkgdesc.py
@@ -5,17 +5,11 @@ KEY_RE = r"^%([A-Z]+)%$"
 
 
 def package_desc_read(file: TextIO) -> dict[str, list[str]]:
-    no = 0
     info = {}
-    to_skip = 0
     key = None
     value: list[str] = []
     for line in file.readlines():
         line = line[:-1]
-        no += 1
-        if to_skip > 0:
-            to_skip -= 1
-            continue
         if key is None:
             m = re.match(KEY_RE, line)
             if m is None:

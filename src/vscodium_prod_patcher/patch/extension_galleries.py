9fe9faaa565503b88b3fe415f6b98b438456d31a
fnrir <fnr1r0@protonmail.com>
2025-06-22 22:33:39 +0200

    feat: update open-vsx gallery info

--- src/vscodium_prod_patcher/patch/extension_galleries.py
+++ src/vscodium_prod_patcher/patch/extension_galleries.py
@@ -1,6 +1,10 @@
+# pylint: disable=line-too-long
+
 EXTENSIONS_OPENVSX_GALLERY = {
     "serviceUrl": "https://open-vsx.org/vscode/gallery",
     "itemUrl": "https://open-vsx.org/vscode/item",
+    "extensionUrlTemplate": "https://open-vsx.org/vscode/gallery/{publisher}/{name}/latest",  # noqa: E501
+    "controlUrl": "https://raw.githubusercontent.com/EclipseFdn/publish-extensions/refs/heads/master/extension-control/extensions.json",  # noqa: E501
 }
 EXTENSIONS_OPENVSX_TRUSTED = ["https://open-vsx.org"]
 
